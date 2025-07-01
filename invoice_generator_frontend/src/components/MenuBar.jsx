a8159a2016f0843c17a205f651255c23b81fb9c5
Lokesh Saini <lokeshsainivri2001@gmail.com>
2025-07-01 11:54:06 +0530

    Create log component and create navbar

--- invoice_generator_frontend/src/components/MenuBar.jsx
+++ invoice_generator_frontend/src/components/MenuBar.jsx
@@ -1,6 +1,19 @@
+
+import { Link } from "react-router-dom";
+import Logo from "./logo";
+
 const MenuBar = ()=>{
     return(
-        <div>Menu Bar</div>
-    )
+     <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm sticky-top">
+      <div className="container py-2">
+        <Link className="navbar-brand d-flex align-items-center" to="/">
+         <Logo/>
+         <span className="fw-bolder fs-4 mx-3" style={{letterSpacing:'0.5px', color :'#0D6EFDB2'}}>
+          QuickInvoice
+         </span>
+        </Link>
+      </div>
+     </nav>
+ )
 }
 export default MenuBar;
