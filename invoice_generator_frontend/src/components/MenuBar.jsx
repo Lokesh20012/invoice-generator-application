dfb0296cf70f6b3a8327ceca579da892a82e9500
Lokesh Saini <lokeshsainivri2001@gmail.com>
2025-07-08 22:22:17 +0530

    Update in menubar

--- invoice_generator_frontend/src/components/MenuBar.jsx
+++ invoice_generator_frontend/src/components/MenuBar.jsx
@@ -1,28 +1,17 @@
 import { Link } from "react-router-dom";
-import Logo from "./Logo";
+import Logo from "./logo";
 
 const MenuBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm sticky-top">
       <div className="container py-2">
         <Link className="navbar-brand d-flex align-items-center" to="/">
-          <Logo />
+          <Logo/>
           <span className="fw-bolder fs-4 mx-3" style={{ letterSpacing: '0.5px', color: '#0D6EFDB2' }}>
             QuickInvoice
           </span>
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation">
-
-          <span className="navbar-toggler-icon"></span>
-
-        </button>
+        
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto align-items-center">
 
