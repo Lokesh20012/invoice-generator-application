9a943471db453ce0ebb01221ae2ac7d1808e5fd5
Lokesh Saini <lokeshsainivri2001@gmail.com>
2025-07-19 11:53:28 +0530

    modified

--- invoice_generator_frontend/src/components/TemplateGrid.jsx
+++ invoice_generator_frontend/src/components/TemplateGrid.jsx
@@ -5,7 +5,7 @@ const TemplatGrid = () => {
     return(
        <div className="row g-3">
         {
-            // used map for iterate the objects in template array and return
+            // used map for iterate, first extract the objects in template array and return
                 templates.map(({id,label,image})=>(
                     <div key={id} className="col-12 col-sm-4 col-lg-4">
                     <div className="border-rounded shadow-sm overflow-hidden template-hover cursor-pointer" title={label}>
